06a5c8f05a410b84ebdd212de1f290d14320ceb6
Alexander Dolgan <5704737@gmail.com>
2015-12-01 10:40:29 +0300

    radio

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,8 +1,11 @@
 
 //Trianglify background generator
 
+
 var pattern = Trianglify({
     width: window.innerWidth,
-    height: 650
+    height: 650,
+    cell_size: 40,
+    x_colors: 'random'
 });
 document.body.appendChild(pattern.canvas());
